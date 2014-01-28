3638909f2d4028f20c9a8ab8f6adcedbe3c56d56
Patrick Madden <maddenpj@gmail.com>
2014-01-28 15:34:40 -0600

    Add order source for testing and development

---
+++ TestOrderSource.js
@@ -0,0 +1,33 @@
+var SellOrder = require('./Order.js');
+var EventEmitter = require('events').EventEmitter;
+
+function TestOrderSource(period, file) {
+  this.period = period;
+  this.file = file || "./test_data/all_offers.20140128.json"
+}
+TestOrderSource.prototype = Object.create(EventEmitter.prototype);
+
+TestOrderSource.prototype.start = function() {
+  var book = this.cleanOrders(JSON.parse(require('fs').readFileSync(this.file)))
+  var self = this;
+  setInterval(function() {
+    self.emit("update", book)
+  }, this.period);
+}
+
+TestOrderSource.prototype.cleanOrders = function(orders) {
+  cleaned = [];
+  for(var i = 0; i < orders.length; i++) {
+    var order = orders[i];
+    if(order.amount_desired == 0) continue;
+    var sellOrder = new SellOrder(order.tx_id, order.address, order.amount, order.amount_desired);
+    cleaned.push(sellOrder);
+  }
+  return cleaned;
+}
+
+module.exports = TestOrderSource;
+
+var ts = new TestOrderSource(10000);
+ts.start();
+ts.on("update", function(orders) { console.log(orders) } );
